4aaa3184e01bc5c9deddfec5df8ff98baaa15865
Paweł Kromołowski <cater981pl@gmail.com>
2022-10-31 16:29:37 +0100

    search section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 import styles from './page.module.css';
 
-export default function Home() {
-  return <main className={styles.main}></main>;
-}
+const Home = () => {
+  return (
+    <main className={styles.main}>
+      <h2 className={styles.header}>Search Character</h2>
+      <p className={styles.text}>
+        Search character by typing his partial or full name.
+      </p>
+      <input type='text' placeholder='Name' className={styles.input} />
+      <div className={styles.buttons}>
+        <button className={styles.button}>Search</button>
+        <button className={styles.button}>Get Random</button>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
