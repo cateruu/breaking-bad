ee9680f4ca3e3891fe2900b84040e52adf14379e
Paweł Kromołowski <cater981pl@gmail.com>
2022-10-31 15:47:23 +0100

    nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import styles from './layout.module.css';
 import './globals.css';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
@@ -8,7 +10,24 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <meta name='description' content='Breaking bed info mordo' />
         <link rel='icon' href='/favicon.ico' />
       </head>
-      <body>{children}</body>
+      <body>
+        <header className={styles.header}>
+          <h1 className={styles.name}>Barking Bed</h1>
+          <nav className={styles.nav}>
+            <Link href='/' className={styles.link}>
+              Search
+            </Link>
+            <Link href='/killers' className={styles.link}>
+              Killers
+            </Link>
+          </nav>
+        </header>
+        {children}
+        <footer>
+          Made by <a href='https://github.com/cateruu'>Paweł</a> and{' '}
+          <a href='https://github.com/chmery'>Wiktor</a>
+        </footer>
+      </body>
     </html>
   );
 };
