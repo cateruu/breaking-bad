14375f0cd3fe771f2e786cc35d6c3b18be22dddc
chmery <karektv@gmail.com>
2022-10-31 20:21:40 +0100

    Add responsiveness to the landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,42 +1,44 @@
-import Link from 'next/link';
-import styles from './layout.module.css';
-import './SearchInput.module.css';
-import './globals.css';
+import Link from "next/link";
+import styles from "./layout.module.css";
+import "./SearchInput.module.css";
+import "./globals.css";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <html lang='en'>
-      <head>
-        <title>Barking Bed</title>
-        <meta name='description' content='Breaking bed info mordo' />
-        <link rel='icon' href='/favicon.ico' />
-      </head>
-      <body>
-        <header className={styles.header}>
-          <h1 className={styles.name}>Barking Bed</h1>
-          <nav className={styles.nav}>
-            <Link href='/' className={styles.link}>
-              Search
-            </Link>
-            <Link href='/killers' className={styles.link}>
-              Top Killers
-            </Link>
-          </nav>
-        </header>
-        {children}
-        <footer className={styles.footer}>
-          Made by{' '}
-          <a href='https://github.com/cateruu' target='_blank' rel='noreferrer'>
-            Paweł
-          </a>{' '}
-          and{' '}
-          <a href='https://github.com/chmery' target='_blank' rel='noreferrer'>
-            Wiktor
-          </a>
-        </footer>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <head>
+                <title>Barking Bed</title>
+                <meta name="description" content="Breaking bed info mordo" />
+                <link rel="icon" href="/favicon.ico" />
+            </head>
+            <body>
+                <header className={styles.header}>
+                    <div>
+                        <h1 className={styles.name}>Barking Bed</h1>
+                        <nav className={styles.nav}>
+                            <Link href="/" className={styles.link}>
+                                Search
+                            </Link>
+                            <Link href="/killers" className={styles.link}>
+                                Top Killers
+                            </Link>
+                        </nav>
+                    </div>
+                </header>
+                {children}
+                <footer className={styles.footer}>
+                    Made by{" "}
+                    <a href="https://github.com/cateruu" target="_blank" rel="noreferrer">
+                        Paweł
+                    </a>{" "}
+                    and{" "}
+                    <a href="https://github.com/chmery" target="_blank" rel="noreferrer">
+                        Wiktor
+                    </a>
+                </footer>
+            </body>
+        </html>
+    );
 };
 
 export default RootLayout;
